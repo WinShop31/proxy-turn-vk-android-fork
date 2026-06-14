dd45f39257cd681e95310ddc788c7abd4d6d09c2
SpaceNeuroX <totoshkus@gmail.com>
2026-06-14 13:27:33 +0200

    chore: bump version to 1.0.8 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "net.qwdtt.client"
         minSdk = 29
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
